2414325f9280e28418dfc1eb42ba2a99f4a47e65
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-06-15 22:05:04 +0200

    Changed starting activity, added listView Now ListView should be displayed when items will be populated

--- app/src/main/java/com/example/memefriends/MainActivity.java
+++ app/src/main/java/com/example/memefriends/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
