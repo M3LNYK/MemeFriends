8c0377291344d1a2734b3d198187a4dbe2164f4c
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-06-16 16:32:23 +0200

    Added FABs and corresponding animation For current FABs following files were added: - animation for rotation opening and closure - animation for dropping down - animation for emerging from bottom - icons for each FAB - text for textViews was moved to strings.xml

--- app/src/main/java/com/example/memefriends/FriendsList.java
+++ app/src/main/java/com/example/memefriends/FriendsList.java
@@ -3,12 +3,93 @@ package com.example.memefriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 
 public class FriendsList extends AppCompatActivity {
+
+    //    can replace this, with -> getApplicationContext()
+    private Animation rotateOpen;
+    private Animation rotateClose;
+    private Animation fromBottom;
+    private Animation toBottom;
+    private Boolean clicked = false;
+
+    private FloatingActionButton fabAdd, fabReaction, fabFriend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+        fabAdd = findViewById(R.id.fab_add);
+        fabReaction = findViewById(R.id.add_reaction_fab);
+        fabFriend = findViewById(R.id.add_person_fab);
+
+        rotateOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_open_animation);
+        rotateClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_close_animation);
+        fromBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.from_bottom_anim);
+        toBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_bottom_anim);
+
+
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddButtonClicked();
+            }
+
+        });
+
+        fabReaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FriendsList.this, "Reaction Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fabFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FriendsList.this, "new Friend clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+
+    private void onAddButtonClicked() {
+        setVisibility(clicked);
+        setAnimation(clicked);
+        if (!clicked) clicked = true;
+        else clicked = false;
+    }
+
+    private void setAnimation(Boolean clicked) {
+        if (!clicked) {
+            fabReaction.startAnimation(fromBottom);
+            fabFriend.startAnimation(fromBottom);
+            fabAdd.startAnimation(rotateOpen);
+        } else {
+            fabReaction.startAnimation(toBottom);
+            fabFriend.startAnimation(toBottom);
+            fabAdd.startAnimation(rotateClose);
+        }
+
+    }
+
+    private void setVisibility(Boolean clicked) {
+        if (!clicked) {
+            fabReaction.setVisibility(View.VISIBLE);
+            fabFriend.setVisibility(View.VISIBLE);
+        } else {
+            fabReaction.setVisibility(View.INVISIBLE);
+            fabFriend.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
 }
