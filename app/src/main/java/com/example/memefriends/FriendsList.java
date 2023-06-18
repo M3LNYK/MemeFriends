b81d10cefd70446a48a33ae10aac49442510ee31
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-06-18 23:06:01 +0200

    Added simple popup display

--- app/src/main/java/com/example/memefriends/FriendsList.java
+++ app/src/main/java/com/example/memefriends/FriendsList.java
@@ -3,11 +3,16 @@ package com.example.memefriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,11 +41,14 @@ public class FriendsList extends AppCompatActivity {
 
     ListView listView;
 
+    View addFriendPopUp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_friends_list);
+
         fabAdd = findViewById(R.id.fab_add);
         fabReaction = findViewById(R.id.add_reaction_fab);
         fabFriend = findViewById(R.id.add_person_fab);
@@ -75,6 +83,7 @@ public class FriendsList extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(FriendsList.this, "new Friend clicked", Toast.LENGTH_SHORT).show();
+                onButtonShowPopupWindowClick(view);
             }
         });
 
@@ -179,6 +188,33 @@ public class FriendsList extends AppCompatActivity {
             textReaction.setVisibility(View.INVISIBLE);
             textFriend.setVisibility(View.INVISIBLE);
         }
+    }
+
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_add_friend, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
 
