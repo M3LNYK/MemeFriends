0745e96ae3825dbde2e83d433bd5ed076bce73bc
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-07-14 23:29:48 +0200

    Add GroupedFriendTest class

---
+++ app/src/test/java/com/example/memefriends/GroupedFriendTest.java
@@ -0,0 +1,22 @@
+package com.example.memefriends;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GroupedFriendTest {
+
+    @Test
+    public void getFirstLetter() {
+    }
+
+    @Test
+    public void getFriends() {
+    }
+
+    @Before
+    public void setUp(){
+        
+    }
+}
