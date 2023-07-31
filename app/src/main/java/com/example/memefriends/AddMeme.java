fde6578985042a5e6be7ff5049440e41e33f9d74
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-07-31 23:44:11 +0200

    Add basic dropdown to friend selection

--- app/src/main/java/com/example/memefriends/AddMeme.java
+++ app/src/main/java/com/example/memefriends/AddMeme.java
@@ -3,15 +3,31 @@ package com.example.memefriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class AddMeme extends AppCompatActivity {
 
     private Button addFunnyMeme, addNotFunnyMeme;
+    private AutoCompleteTextView friendName, memeSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meme);
+
+        friendName = findViewById(R.id.dropdown_friend_name);
+
+        // Replace this array with your list of items for the dropdown menu
+        String[] items = {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                items
+        );
+
+        friendName.setAdapter(adapter);
     }
 }
