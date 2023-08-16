24e9342adfa1c0d9ccbab29495cd8200dd855e76
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-08-16 19:52:51 +0200

    Add popup to confirm friend deletion

--- app/src/main/java/com/example/memefriends/FriendMemes.java
+++ app/src/main/java/com/example/memefriends/FriendMemes.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -144,6 +145,29 @@ public class FriendMemes extends AppCompatActivity {
             enableEditing(outlinedFriendName);
             animateButtons(true);
             Toast.makeText(this, "Now you can change friend name", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (item.getItemId() == R.id.menu_item_delete_friend) {
+            Toast.makeText(this, "Delete friend", Toast.LENGTH_SHORT).show();
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            final View addMemePopupView = getLayoutInflater().inflate(R.layout.popup_delete_friend_confirm, null);
+            builder.setView(addMemePopupView)
+                    .setTitle("Delete friend?")
+                    .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Handle user input
+                            Toast.makeText(FriendMemes.this, "Confirmed deletion", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();
             return true;
         }
         return super.onOptionsItemSelected(item);
