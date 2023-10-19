10b630c4524c3168c4ff9bc55bacf2b1dec97b9b
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-10-19 14:20:35 +0200

    feat: Add initialization for tvs and lls in Help

--- app/src/main/java/com/m3lnyk/memefriends/Help.java
+++ app/src/main/java/com/m3lnyk/memefriends/Help.java
@@ -3,11 +3,17 @@ package com.m3lnyk.memefriends;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Help extends AppCompatActivity {
 
     int versionCode = BuildConfig.VERSION_CODE;
     String versionName = BuildConfig.VERSION_NAME;
+
+    TextView versionNum;
+    LinearLayout contactDev, licenses, guide, sourceCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,24 @@ public class Help extends AppCompatActivity {
         setContentView(R.layout.activity_help);
         setTitle("Help");
 
+        initializeUi();
+        setClickListeners();
+    }
+
+    private void initializeUi() {
+        contactDev = findViewById(R.id.ll_contact_developer);
+        licenses = findViewById(R.id.ll_licenses);
+        guide = findViewById(R.id.ll_guide);
+        sourceCode = findViewById(R.id.ll_source_code);
+        versionNum = findViewById(R.id.tv_version_number);
+        versionNum.setText(versionName);
+    }
+
+    private void setClickListeners() {
+        contactDev.setOnClickListener(view -> onContactDevClicked());
+    }
+
+    private void onContactDevClicked() {
+        Toast.makeText(this, "Contact Dev Clicked", Toast.LENGTH_SHORT).show();
     }
 }
