73f3b26d1333d1d4122de198752e6415fc83e1fb
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-10-24 18:49:55 +0200

    test: add test for friend write and get

--- app/src/androidTest/java/com/m3lnyk/memefriends/roomDb/Friend/FriendDatabaseTest.java
+++ app/src/androidTest/java/com/m3lnyk/memefriends/roomDb/Friend/FriendDatabaseTest.java
@@ -1,31 +1,29 @@
 package com.m3lnyk.memefriends.roomDb.Friend;
 
 
+import static com.google.common.truth.Truth.assertThat;
+
 import android.content.Context;
 
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Objects;
-
 @RunWith(AndroidJUnit4.class)
-public class FriendDatabaseTest extends TestCase {
+public class FriendDatabaseTest{
 
     private FriendDatabase friendDb;
     private FriendDao friendDao;
 
     @Before
-    @Override
-    public void setUp() {
+    public void createDB() {
         Context context = ApplicationProvider.getApplicationContext();
-        friendDb = Room.inMemoryDatabaseBuilder(context, friendDb.getClass()).build();
+        friendDb = Room.inMemoryDatabaseBuilder(context, FriendDatabase.class).build();
         friendDao = friendDb.friendDao();
     }
 
@@ -35,11 +33,11 @@ public class FriendDatabaseTest extends TestCase {
     }
 
     @Test
-    public void writeAndReadFriend(){
+    public void writeFriendAndGetById(){
         Friend friend = new Friend("A1", 5, 3, 2, 99);
         friendDao.insert(friend);
-        Friend res = Objects.requireNonNull(friendDao.getAllFriends().getValue()).get(0);
-        assertSame(res, friend);
+        Friend res = friendDao.getFriendById(0).getValue();
+        assertThat(res == friend);
     }
 
 }
