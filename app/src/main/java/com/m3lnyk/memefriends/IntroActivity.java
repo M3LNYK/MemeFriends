6a50345cfb1e7aace6310234d936fd526c77d422
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-04 00:52:09 +0100

    feat: add check for time running in IntroActivity

--- app/src/main/java/com/m3lnyk/memefriends/IntroActivity.java
+++ app/src/main/java/com/m3lnyk/memefriends/IntroActivity.java
@@ -1,7 +1,9 @@
 package com.m3lnyk.memefriends;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,8 +17,21 @@ public class IntroActivity extends AppIntro {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkIfFirstRun();
+    }
 
+    private void checkIfFirstRun() {
+        if (appGetFirstTimeRun() == 0) {
+            createFirstTimeSlideScreen();
+        } else if ((appGetFirstTimeRun() == 1)) {
+            Intent myIntent = new Intent(IntroActivity.this, FriendsList.class);
+            IntroActivity.this.startActivity(myIntent);
+        } else {
+            Toast.makeText(this, "new version lol", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    private void createFirstTimeSlideScreen() {
         addSlide(AppIntroFragment.createInstance("Welcome!",
                 "This is a demo example in java of AppIntro library, with a custom background on each slide!",
                 R.drawable.ic_slide1,
@@ -49,22 +64,22 @@ public class IntroActivity extends AppIntro {
 
         // Show/hide status bar
         showStatusBar(true);
-        //Enable the color "fade" animation between two slides (make sure the slide implements SlideBackgroundColorHolder)
+        // Enable the color "fade" animation between two slides (make sure the slide implements SlideBackgroundColorHolder)
         setColorTransitionsEnabled(true);
 
-        //Prevent the back button from exiting the slides
+        // Prevent the back button from exiting the slides
         setSystemBackButtonLocked(true);
 
-        //Activate wizard mode (Some aesthetic changes)
+        // Activate wizard mode (Some aesthetic changes)
         setWizardMode(true);
 
-        //Enable immersive mode (no status and nav bar)
+        // Enable immersive mode (no status and nav bar)
         setImmersiveMode();
 
-        //Enable/disable page indicators
+        // Enable/disable page indicators
         setIndicatorEnabled(true);
 
-        //Dhow/hide ALL buttons
+        // Dhow/hide ALL buttons
         setButtonsEnabled(true);
 
         // Enable Vibration
@@ -85,5 +100,27 @@ public class IntroActivity extends AppIntro {
         Intent myIntent = new Intent(IntroActivity.this, FriendsList.class);
         IntroActivity.this.startActivity(myIntent);
         finish();
+    }
+
+    private int appGetFirstTimeRun() {
+        // Check if App Start First Time
+        SharedPreferences appPreferences = getSharedPreferences("MyAPP", 0);
+        int appCurrentBuildVersion = BuildConfig.VERSION_CODE;
+        int appLastBuildVersion = appPreferences.getInt("app_first_time", 0);
+
+        // Log.d("appPreferences", "app_first_time = " + appLastBuildVersion);
+
+        if (appLastBuildVersion == appCurrentBuildVersion) {
+            return 1; // It has started ever
+
+        } else {
+            appPreferences.edit().putInt("app_first_time",
+                    appCurrentBuildVersion).apply();
+            if (appLastBuildVersion == 0) {
+                return 0; // If result is first time
+            } else {
+                return 2; //  It has started once, but not that version , ie it is an update.
+            }
+        }
     }
 }
