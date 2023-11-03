51a1c45bf302c26b4bece302e0d246f8fb4ee8ad
M3LNYK <48330869+M3LNYK@users.noreply.github.com>
2023-11-03 16:26:39 +0100

    feat: add AppIntro demo implementation

---
+++ app/src/main/java/com/m3lnyk/memefriends/IntroActivity.java
@@ -0,0 +1,89 @@
+package com.m3lnyk.memefriends;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.github.appintro.AppIntro;
+import com.github.appintro.AppIntroFragment;
+import com.github.appintro.AppIntroPageTransformerType;
+
+public class IntroActivity extends AppIntro {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        addSlide(AppIntroFragment.createInstance("Welcome!",
+                "This is a demo example in java of AppIntro library, with a custom background on each slide!",
+                R.drawable.ic_slide1,
+                R.color.appintro_example_orange
+        ));
+
+        addSlide(AppIntroFragment.createInstance(
+                "Clean App Intros",
+                "This library offers developers the ability to add clean app intros at the start of their apps.",
+                R.drawable.ic_slide2,
+                R.color.appintro_example_lime
+        ));
+
+        addSlide(AppIntroFragment.createInstance(
+                "Simple, yet Customizable",
+                "The library offers a lot of customization, while keeping it simple for those that like simple.",
+                R.drawable.ic_slide3,
+                R.color.appintro_example_blue
+        ));
+
+        addSlide(AppIntroFragment.createInstance(
+                "Explore",
+                "Feel free to explore the rest of the library demo!",
+                R.drawable.ic_slide4,
+                R.color.appintro_example_blue_gray
+        ));
+
+        // Fade Transition
+        setTransformer(AppIntroPageTransformerType.Fade.INSTANCE);
+
+        // Show/hide status bar
+        showStatusBar(true);
+        //Enable the color "fade" animation between two slides (make sure the slide implements SlideBackgroundColorHolder)
+        setColorTransitionsEnabled(true);
+
+        //Prevent the back button from exiting the slides
+        setSystemBackButtonLocked(true);
+
+        //Activate wizard mode (Some aesthetic changes)
+        setWizardMode(true);
+
+        //Enable immersive mode (no status and nav bar)
+        setImmersiveMode();
+
+        //Enable/disable page indicators
+        setIndicatorEnabled(true);
+
+        //Dhow/hide ALL buttons
+        setButtonsEnabled(true);
+
+        // Enable Vibration
+        // setVibrate(true);
+    }
+
+    @Override
+    protected void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent myIntent = new Intent(IntroActivity.this, FriendsList.class);
+        IntroActivity.this.startActivity(myIntent);
+        finish();
+    }
+
+    @Override
+    protected void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent myIntent = new Intent(IntroActivity.this, FriendsList.class);
+        IntroActivity.this.startActivity(myIntent);
+        finish();
+    }
+}
